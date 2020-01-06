12fe3a350efa5dc63bcfaedb232f1890194d83b6
Mark Francis <mrkfrancis8@gmail.com>
2020-01-06 10:29:47 +0000

    Sample integration with Google Sheets API

---
+++ GoogleSheets.HelloWorld/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace GoogleSheets.HelloWorld
+{
+    using static System.Environment;
+
+    public static class AppSettings
+    {
+        public const string _sheetsApiKey = "SHEETS_API_KEY";
+        public static string SheetsApiKey => GetEnvironmentVariable(_sheetsApiKey);
+
+        public const string _sheetsDocId = "SHEETS_DOC_ID";
+        public static string SheetsDocId => GetEnvironmentVariable(_sheetsDocId);
+    }
+}
--- GoogleSheets.HelloWorld/Program.cs
+++ GoogleSheets.HelloWorld/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using System;
+using System.Threading;
 
 namespace GoogleSheets.HelloWorld
 {
     class Program
     {
+        static readonly string[] _scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        public const string _appName = "GoogleSheets.HelloWorld";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var apiKey = AppSettings.SheetsApiKey;
+            var docId = AppSettings.SheetsDocId;
+
+            var service = new SheetsService(new BaseClientService.Initializer
+            {
+                ApiKey = apiKey,
+                ApplicationName = _appName
+            });
+
+            var request = service.Spreadsheets.Values.Get(docId, "A:B");
+
+            var spreadsheet = request.Execute();
+
+            var sheetData = spreadsheet.Values;
+            foreach (var row in sheetData)
+            {
+                foreach (var cell in row)
+                {
+                    Console.WriteLine(cell);
+                }
+            }
+            Console.WriteLine();
+            Console.ReadKey(true);
         }
     }
 }
